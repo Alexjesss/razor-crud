1c527afbd3bd3db65b875e30411814f2b1042379
Alexandra Banica <banica9221@hotmail.be>
2021-04-01 18:20:04 +0200

    all models present,only styling now

--- RazorCrud/Models/SeedData.cs
+++ RazorCrud/Models/SeedData.cs
@@ -14,12 +14,9 @@ namespace RazorCrud.Models
             using (var context = new RazorCrudActorContext(serviceProvider.GetRequiredService<DbContextOptions<RazorCrudActorContext>>()))
             {
                 // Look for any actors.
-                if (context.Actor.Any())
+                if (!context.Actor.Any())
                 {
-                    return;   // DB has been seeded
-                }
-
-                context.Actor.AddRange(
+                    context.Actor.AddRange(
                     new Actor
                     {
                         Name = "Philip Seymour Hoffman",
@@ -49,6 +46,71 @@ namespace RazorCrud.Models
                     }
                 );
                 context.SaveChanges();
+            }
+            }
+
+            using (var context = new RazorCrudActriceContext(serviceProvider.GetRequiredService<DbContextOptions<RazorCrudActriceContext>>()))
+            {
+                // Look for any actors.
+                if (!context.Actrice.Any())
+                {
+
+                    context.Actrice.AddRange(
+                    new Actrice
+                    {
+                        Name = "Alison Brie",
+                        Movie = "Glow",
+                        ReleaseDate = DateTime.Parse("2017-06-23"),
+                    },
+
+                    new Actrice
+                    {
+                        Name = "Vera Farmiga",
+                        Movie = "Bates Motel",
+                        ReleaseDate = DateTime.Parse("2013-03-18"),
+                    },
+
+                    new Actrice
+                    {
+                        Name = "Jenifer Aniston",
+                        Movie = "Friends",
+                        ReleaseDate = DateTime.Parse("1994-09-22"),
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+
+            using (var context = new RazorCrudDirectorContext(serviceProvider.GetRequiredService<DbContextOptions<RazorCrudDirectorContext>>()))
+            {
+                // Look for any actors.
+                if (!context.Director.Any())
+                {
+
+                    context.Director.AddRange(
+                    new Director
+                    {
+                        Name = "Darren Aronofsky ",
+                        Movie = "Requiem for a dream",
+                        ReleaseDate = DateTime.Parse("2000-10-06"),
+                    },
+
+                    new Director
+                    {
+                        Name = "Alfred Hitchcock",
+                        Movie = "Psycho",
+                        ReleaseDate = DateTime.Parse("1960-09-08"),
+                    },
+
+                    new Director
+                    {
+                        Name = "Stanley Kubrick",
+                        Movie = "The Shining",
+                        ReleaseDate = DateTime.Parse("1980-06-13"),
+                    }
+                    );
+                    context.SaveChanges();
+                }
             }
 
 
